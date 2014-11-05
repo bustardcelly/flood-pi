e04f9b5cbedaca58b28363f42cbae9cb25b0a6e8
Todd Anderson <bustardcelly@gmail.com>
2014-11-05 16:25:34 -0500

    fixes and update to setup in README.

--- README.md
+++ README.md
@@ -15,6 +15,14 @@ $ sudo pip install virtualenvwrapper
 - update .bashrc as described here: [http://virtualenvwrapper.readthedocs.org/en/latest/install.html](http://virtualenvwrapper.readthedocs.org/en/latest/install.html)
 (had to change __virtuelenvwrapper.sh__ location to _$HOME/build/virtualenvwrapper/virtualenvwrapper.sh_)
 
+Allow SPI interface & Reboot
+---
+As described here: [http://www.raspberrypi-spy.co.uk/2013/10/analogue-sensors-on-the-raspberry-pi-using-an-mcp3008/](http://www.raspberrypi-spy.co.uk/2013/10/analogue-sensors-on-the-raspberry-pi-using-an-mcp3008/)
+
+__Actually had to follow instruction for local install of spidev - the one from pip isn't recognized for some reason__
+
+Install
+---
 ```
 $ git clone https://github.com/bustardcelly/flood-pi.git flood-pi
 $ cd flood-pi
@@ -22,5 +30,10 @@ $ mkvirtualenv flood-pi
 
 $ workon flood-pi
 $ pip install -r requirements.txt --system-site-packages
-$ sudo python flood-pi.py
+```
+
+Start
+---
+```
+$ sudo python floodpi.py -n bustardcelly@gmail.com
 ```
--- floodpi.py
+++ floodpi.py
@@ -1,11 +1,14 @@
+import time
 import argparse
+
 from floodpi.control.mcp3008 import ADC
 from floodpi.service.notifier import SMTPNotifier
 
 import RPi.GPIO as GPIO
 
-MIN_THRESHOLD=400
-MAX_THRESHOLD=700
+# 5.5, 100ohm resister
+MIN_THRESHOLD=300
+MAX_THRESHOLD=500
 
 READ_SLEEP=0.5
 
@@ -20,9 +23,12 @@ class Unpack(object):
   pass
 
 def check_flood():
+  global adc
   return adc.readadc(flood_adc)
 
 def flood_watch(notifiees):
+  global adc
+
   running = True
   
   adc = ADC()
@@ -33,9 +39,10 @@ def flood_watch(notifiees):
   while running:
     try:
       level = check_flood()
+      print "level %r" % level
       if level > MIN_THRESHOLD and level < MAX_THRESHOLD:
         print "Detected flood... %r" % level
-        notifier.run(notifies)
+        notifier.run(notifiees, level)
       time.sleep(READ_SLEEP)
     except KeyboardInterrupt:
       running = False
@@ -43,6 +50,7 @@ def flood_watch(notifiees):
       sys.exit('\nExplicit close.')
 
 if __name__ == '__main__':
+  GPIO.setmode(GPIO.BCM)
   notify_list = []
   unpack = Unpack()
   args = parser.parse_args(namespace=unpack)
